059bcbdc12947129addd92f12cf6bb2fa50e536c
Tanaka Chingonzo <tchingonzo21@wooster.edu>
2019-04-09 19:12:20 -0400

    username methods documentation

--- username.py
+++ username.py
@@ -1,5 +1,5 @@
 import praw
-
+# authentication token for reddit throwaway account
 reddit = praw.Reddit(client_id='bFbCvelvrdVuFA',
                      client_secret='HjJOdVpi_jERV4T8PhAmxwtEM6w',
                      password='CaramelApple',
@@ -8,6 +8,11 @@ reddit = praw.Reddit(client_id='bFbCvelvrdVuFA',
 
 
 def get_redditor_info(redditusername):
+    """
+    get_redditor_info() function pulls user public attributes from reddit redditor object
+    :param redditusername
+    :return: redditor_profile - user account profile string object with link and comment karma
+    """
     redditor_link_karmar = str(reddit.redditor(redditusername).link_karma)
     redditor_name = reddit.redditor(redditusername).name
     redditor_comment_karma = str(reddit.redditor(redditusername).comment_karma)
