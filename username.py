b4ea87f250d2bd51b3ba84dd120b1b225e6be958
Tanaka Chingonzo <tchingonzo21@wooster.edu>
2019-04-09 18:51:32 -0400

    username function

--- username.py
+++ username.py
@@ -1,10 +1,9 @@
 import praw
 
-reddit = praw.Reddit(client_id='5gcyUe_9oxJjBQ',
-                         client_secret='wDZk9NXJrUTUJqu4yh3q1y8P01g',
-                         password='VjGT4m5Uh7AQ',
-                         user_agent='testapp',
-                         username='progres5ion')
+reddit = praw.Reddit(client_id='5gcyUe_9oxJjBQ', client_secret='wDZk9NXJrUTUJqu4yh3q1y8P01g',
+                     password='VjGT4m5Uh7AQ',
+                     user_agent='testapp',
+                     username='progres5ion')
 
 
 def get_redditor_info(redditusername):
@@ -15,5 +14,4 @@ def get_redditor_info(redditusername):
     redditor_profile = " "
     redditor_profile += "Redditor Name: " + redditor_name + "\n Redditor Comment Karma: " + redditor_comment_karma +\
                         "\n Link Karma: " + redditor_link_karmar
-
     return redditor_profile
